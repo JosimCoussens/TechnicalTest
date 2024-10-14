728caec78c0194f52bf1568d1bd4fdf1eef4c442
JosimCoussens <coussensjosim@yahoo.com>
2024-10-14 20:24:49 +0200

    Stable version. Can get a list of all records

--- technical_test/app/todos.tsx
+++ technical_test/app/todos.tsx
@@ -1,45 +1,7 @@
-("use client");
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-export default function GetTodos() {
-  // Hooks
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    fetch("https://dummyjson.com/todos")
-      .then((res) => res.json()) // Convert to JS object
-      .then((data) => {
-        setTodos(data.todos);
-      });
-  });
+const todos = () => {
+  return <p></p>;
+};
 
-  const listTodos = () => {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        <ul>
-          {todos.map((todo) => (
-            <div
-              style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(5, 1fr)", // 4 equal columns
-                gap: "10px", // space between grid items
-                marginBottom: "10px", // spacing between todo items
-                padding: "10px",
-                border: "1px solid #ccc", // border for each todo item
-                borderRadius: "5px",
-              }}
-            >
-              <p>{todo.id}</p>
-              <p>{todo.todo}</p>
-              <p>{todo.completed ? "Completed" : "Not Completed"}</p>
-              <p>{todo.userId}</p>
-              <button>DELETE</button>
-            </div>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-
-  return listTodos;
-}
+export default todos;
