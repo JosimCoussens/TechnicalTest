1fe3cfe77e8521469d7a08ed62bafc09fab6ebd3
JosimCoussens <coussensjosim@yahoo.com>
2024-10-16 20:34:37 +0200

    - Made new function called handleAdd for delete functionality - Changed handleDelete from async function to normal function - Updated comments

--- technical_test/app/list/page.tsx
+++ technical_test/app/list/page.tsx
@@ -10,27 +10,46 @@ export default function ListPage() {
   // useEffect is a React Hook that lets you synchronize a component with an external system.
   useEffect(() => {
     fetch("https://dummyjson.com/todos")
-      .then((res) => res.json()) // Convert to JS object
+      .then((res) => res.json()) // Convert to object
       .then((data) => {
         setTodos(data.todos);
       });
   }, []); // component mounting: when the array is empty ([]) the useEffect will run only once
 
-  const handleDelete = async (id: any) => {
-    try {
-      // await -> pause execution of an async function
-      const response = await fetch(`https://dummyjson.com/todos/${id}`, {
-        method: "DELETE",
+  const handleDelete = (id: any) => {
+    // Fetch returns a promise, so we handle it with .then()
+    fetch(`https://dummyjson.com/todos/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json()) // Parse the response to JSON
+      .then((result) => {
+        console.log("Deleted:", result);
+        // Update state to remove the deleted item from the UI
+        setTodos(todos.filter((todo) => todo.id != id));
       });
-      const result = await response.json();
+  };
 
-      // Update state to remove the deleted item from the UI
-      setTodos(todos.filter((todo) => todo.id !== id));
+  const handleAdd = () => {
+    fetch("https://dummyjson.com/todos/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        todo: newRecordTodo,
+        completed: newRecordCompleted,
+        userId: 2,
+      }),
+    })
+      .then((res) => res.json())
+      .then((newTodo) => {
+        // Loop over todos array to get max number of id, to create new id
+        let maxValue = 0;
+        for (let i = 0; i < todos.length; i++)
+          if (parseInt(todos[i].id) > maxValue) maxValue = todos[i].id;
 
-      console.log("Deleted:", result);
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-    }
+        newTodo.id = maxValue + 1;
+        setTodos([...todos, newTodo]); // Makes a copy of todos and adds a new record to the array
+        console.log("New todo added:", newTodo);
+      });
   };
 
   return (
@@ -60,33 +79,7 @@ export default function ListPage() {
             }}
           />
           <textarea disabled={true} defaultValue={"userId: always 1"} />
-          <button
-            onClick={() => {
-              fetch("https://dummyjson.com/todos/add", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                  todo: newRecordTodo,
-                  completed: newRecordCompleted,
-                  userId: 1,
-                }),
-              })
-                .then((res) => res.json())
-                .then((newTodo) => {
-                  // Loop over todos array to get max number of id, to create new id
-                  let maxValue = 0;
-                  for (let i = 0; i < todos.length; i++)
-                    if (parseInt(todos[i].id) > maxValue)
-                      maxValue = todos[i].id;
-
-                  newTodo.id = maxValue + 1;
-                  setTodos([...todos, newTodo]); // Makes a copy of todos and adds a new record to the array
-                  console.log("New todo added:", newTodo);
-                });
-            }}
-          >
-            ADD
-          </button>
+          <button onClick={handleAdd}>ADD</button>
         </div>
       </ul>
     </div>
